c39b13329ac679efa38eeacfcb028476c90b636e
thelukemccarthy <thelukemccarthy@gmail.com>
2014-08-22 13:45:30 +0100

    Refactor the lamp_state_machine.py to use the lamp_state.py for the know states of the system.

--- lamp_state_machine.py
+++ lamp_state_machine.py
@@ -1,21 +1,18 @@
 import unittest
-
+from lamp_state import LampState
 
 class LampStateMachine(unittest.TestCase):
-  OFF = 0
-  LOW = 33
-  MEDIUM = 66
-  HIGH = 100
-
   NO_TOUCH_DETECTED = 0
   TOUCH_DETECTED = 1
 
+  __lampState = LampState()
+
   __lamp_state_machine = \
     { 
-      OFF: [OFF, LOW], 
-      LOW: [LOW, MEDIUM], 
-      MEDIUM: [MEDIUM, HIGH], 
-      HIGH: [HIGH, OFF] 
+      __lampState.OFF: [__lampState.OFF, __lampState.LOW], 
+      __lampState.LOW: [__lampState.LOW, __lampState.MEDIUM], 
+      __lampState.MEDIUM: [__lampState.MEDIUM, __lampState.HIGH], 
+      __lampState.HIGH: [__lampState.HIGH, __lampState.OFF] 
     } 
 
   def get_next_state(self, current_state, input_value):
@@ -23,44 +20,44 @@ class LampStateMachine(unittest.TestCase):
     return next_states[input_value]
 
   def test_givenACurrentStateOfOffAndNoTouchDetected_whenGetNextStateIsCalled_thenTheNextLampStateShouldBeOff(self):
-    expected = self.OFF
-    actual = self.get_next_state(self.OFF, self.NO_TOUCH_DETECTED) 
+    expected = self.__lampState.OFF
+    actual = self.get_next_state(self.__lampState.OFF, self.NO_TOUCH_DETECTED) 
     self.assertEqual(expected, actual)
 
   def test_givenACurrentStateOfOffAndATouchDetected_whenGetNextStateIsCalled_thenTheNextLampStateShouldBeLow(self):
-    expected = self.LOW
-    actual = self.get_next_state(self.OFF, self.TOUCH_DETECTED) 
+    expected = self.__lampState.LOW
+    actual = self.get_next_state(self.__lampState.OFF, self.TOUCH_DETECTED) 
     self.assertEqual(expected, actual)
 
 
   def test_givenACurrentStateOfLowAndNoTouchDetected_whenGetNextStateIsCalled_thenTheNextLampStateShouldBeLow(self):
-    expected = self.LOW
-    actual = self.get_next_state(self.LOW, self.NO_TOUCH_DETECTED) 
+    expected = self.__lampState.LOW
+    actual = self.get_next_state(self.__lampState.LOW, self.NO_TOUCH_DETECTED) 
     self.assertEqual(expected, actual)
 
   def test_givenACurrentStateOfLowAndATouchDetected_whenGetNextStateIsCalled_thenTheNextLampStateShouldBeMedium(self):
-    expected = self.MEDIUM
-    actual = self.get_next_state(self.LOW, self.TOUCH_DETECTED) 
+    expected = self.__lampState.MEDIUM
+    actual = self.get_next_state(self.__lampState.LOW, self.TOUCH_DETECTED) 
     self.assertEqual(expected, actual)
 
   def test_givenACurrentStateOfMediumAndNoTouchDetected_whenGetNextStateIsCalled_thenTheNextLampStateShouldBeMedium(self):
-    expected = self.MEDIUM
-    actual = self.get_next_state(self.MEDIUM, self.NO_TOUCH_DETECTED) 
+    expected = self.__lampState.MEDIUM
+    actual = self.get_next_state(self.__lampState.MEDIUM, self.NO_TOUCH_DETECTED) 
     self.assertEqual(expected, actual)
 
   def test_givenACurrentStateOfMediumAndATouchDetected_whenGetNextStateIsCalled_thenTheNextLampStateShouldBeHigh(self):
-    expected = self.HIGH
-    actual = self.get_next_state(self.MEDIUM, self.TOUCH_DETECTED) 
+    expected = self.__lampState.HIGH
+    actual = self.get_next_state(self.__lampState.MEDIUM, self.TOUCH_DETECTED) 
     self.assertEqual(expected, actual)
 
   def test_givenACurrentStateOfHighAndNoTouchDetected_whenGetNextStateIsCalled_thenTheNextLampStateShouldBeHigh(self):
-    expected = self.HIGH
-    actual = self.get_next_state(self.HIGH, self.NO_TOUCH_DETECTED) 
+    expected = self.__lampState.HIGH
+    actual = self.get_next_state(self.__lampState.HIGH, self.NO_TOUCH_DETECTED) 
     self.assertEqual(expected, actual)
 
   def test_givenACurrentStateOfHighAndATouchDetected_whenGetNextStateIsCalled_thenTheNextLampStateShouldBeOff(self):
-    expected = self.OFF
-    actual = self.get_next_state(self.HIGH, self.TOUCH_DETECTED) 
+    expected = self.__lampState.OFF
+    actual = self.get_next_state(self.__lampState.HIGH, self.TOUCH_DETECTED) 
     self.assertEqual(expected, actual)
 
 if __name__ == '__main__':
